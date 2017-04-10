305e4ae96b89b6931221023b5b7c35c41819e2ee
Syrus Akbary <me@syrusakbary.com>
2017-04-10 04:46:03 -0700

    Updated version to 0.2.2

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ tests_require = [
 
 setup(
     name='snapshottest',
-    version='0.2.1',
+    version='0.2.2',
     description='Snapshot Testing utils for Python',
     long_description=readme,
     author='Syrus Akbary',
