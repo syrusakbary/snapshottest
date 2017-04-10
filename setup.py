af8723c2a9f607c3d4e3c54a2401bf7fff4f620f
Syrus Akbary <me@syrusakbary.com>
2017-04-09 20:39:15 -0700

    Fixed classifier

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ setup(
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Libraries',
-        'Programming Language :: Python'
+        'Programming Language :: Python',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
