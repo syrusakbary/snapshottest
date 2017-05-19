a7c6bcd63bd0e199ed997def40dd0117149f3900
Juan Sepulveda <juan@sepulveda.do>
2017-05-19 17:13:56 -0400

    Don't export TestRunner in module

--- snapshottest/__init__.py
+++ snapshottest/__init__.py
@@ -2,8 +2,6 @@ from .snapshot import Snapshot
 from .generic_repr import GenericRepr
 from .module import assert_match_snapshot
 from .unittest import TestCase
-from .django import TestRunner
 
 
-__all__ = ['Snapshot', 'GenericRepr', 'assert_match_snapshot', 'TestCase',
-           'TestRunner']
+__all__ = ['Snapshot', 'GenericRepr', 'assert_match_snapshot', 'TestCase']
