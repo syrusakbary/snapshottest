e4a3d3c273a1b7e26e9fdcf7f8da060bf127f27e
Juan Sepulveda <juan@sepulveda.do>
2017-05-20 01:25:14 -0400

    Add note for test_api_me django-project test

--- examples/django_project/django_project/tests.py
+++ examples/django_project/django_project/tests.py
@@ -13,6 +13,8 @@ def api_client_get(url):
 class TestDemo(TestCase):
 
     def test_api_me(self):
+        # Note this tests should fail unless the snapshot-update command line
+        # option is specified. Run  `python manage.py test --snapshot-update`.
         now = datetime.now().isoformat()
         my_api_response = api_client_get('/' + now)
         self.assertMatchSnapshot(my_api_response)
