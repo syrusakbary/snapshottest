b219f244f2a260ee7f0088bbc0448a83ebe25e57
Matan Gover <mgover@landr.com>
2018-07-06 13:36:29 -0400

    Add demo tests for custom object and file snapshot.

--- examples/pytest/snapshots/snap_test_demo.py
+++ examples/pytest/snapshots/snap_test_demo.py
@@ -2,7 +2,8 @@
 # snapshottest: v1 - https://goo.gl/zC4yUc
 from __future__ import unicode_literals
 
-from snapshottest import Snapshot
+from snapshottest import GenericRepr, Snapshot
+from snapshottest.file import FileSnapshot
 
 
 snapshots = Snapshot()
@@ -11,4 +12,12 @@ snapshots['test_me_endpoint 1'] = {
     'url': '/me'
 }
 
-snapshots['test_unicode 1'] = u'p\xe9p\xe8re'
+snapshots['test_unicode 1'] = 'pépère'
+
+snapshots['test_object 1'] = GenericRepr('SomeObject(3)')
+
+snapshots['test_file 1'] = FileSnapshot('snap_test_demo/test_file 1.txt')
+
+snapshots['test_multiple_files 1'] = FileSnapshot('snap_test_demo/test_multiple_files 1.txt')
+
+snapshots['test_multiple_files 2'] = FileSnapshot('snap_test_demo/test_multiple_files 2.txt')
