3bacb425f9880802d31a268ccf019f49e53d6619
Jae Lee <jae@say.com>
2019-02-08 17:38:52 -0500

    Revert import change
    Ended up breaking imports when updating snapshots

--- snapshottest/module.py
+++ snapshottest/module.py
@@ -25,7 +25,7 @@ class SnapshotModule(object):
         self.visited_snapshots = set()
         self.new_snapshots = set()
         self.failed_snapshots = set()
-        self.imports['snapshottest.snapshot'].add('Snapshot')
+        self.imports['snapshottest'].add('Snapshot')
 
     def load_snapshots(self):
         try:
