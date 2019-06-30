8f9a611fa0e386e01a89d97f8c4e71e0dd8e0f06
Ygor Mutti <ygormutti@gmail.com>
2019-06-29 21:27:29 -0300

    Update snapshots

--- examples/pytest/snapshots/snap_test_demo.py
+++ examples/pytest/snapshots/snap_test_demo.py
@@ -21,3 +21,23 @@ snapshots['test_file 1'] = FileSnapshot('snap_test_demo/test_file 1.txt')
 snapshots['test_multiple_files 1'] = FileSnapshot('snap_test_demo/test_multiple_files 1.txt')
 
 snapshots['test_multiple_files 2'] = FileSnapshot('snap_test_demo/test_multiple_files 2.txt')
+
+snapshots['test_nested_objects 1'] = {
+    'key': GenericRepr('#')
+}
+
+snapshots['test_nested_objects 2'] = [
+    GenericRepr('#')
+]
+
+snapshots['test_nested_objects 3'] = (
+    GenericRepr('#')
+,)
+
+snapshots['test_nested_objects 4'] = set([
+    GenericRepr('#')
+])
+
+snapshots['test_nested_objects 5'] = frozenset([
+    GenericRepr('#')
+])
