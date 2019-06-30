612f5047ac3c474678d542a3c120a1e16b1b6885
Ygor Mutti <ygormutti@gmail.com>
2019-06-29 21:24:17 -0300

    Add failing tests

--- examples/pytest/test_demo.py
+++ examples/pytest/test_demo.py
@@ -58,3 +58,25 @@ def test_multiple_files(snapshot, tmpdir):
     temp_file1 = tmpdir.join('example2.txt')
     temp_file1.write('Hello, world 2!')
     snapshot.assert_match(FileSnapshot(str(temp_file1)))
+
+
+class ObjectWithBadRepr(object):
+    def __repr__(self):
+        return "#"
+
+
+def test_nested_objects(snapshot):
+    obj = ObjectWithBadRepr()
+    
+    dict_ = {'key': obj}
+    list_ = [obj]
+    tuple_ = (obj,)
+    set_ = set((obj,))
+    frozenset_ = frozenset((obj,))
+    
+    snapshot.assert_match(dict_)
+    snapshot.assert_match(list_)
+    snapshot.assert_match(tuple_)
+    snapshot.assert_match(set_)
+    snapshot.assert_match(frozenset_)
+    
