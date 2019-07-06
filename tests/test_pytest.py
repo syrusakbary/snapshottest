b34fbc5ef4ae4a4a14f3848eded465e4bccc2866
Johnny Metz <jmetz@humanlongevity.com>
2019-07-06 14:22:32 -0700

    Fix pytest parametrize for multiline strings

--- tests/test_pytest.py
+++ tests/test_pytest.py
@@ -46,3 +46,23 @@ def test_pytest_snapshottest_property_test_name(pytest_snapshot_test):
     pytest_snapshot_test.assert_match('counter')
     assert pytest_snapshot_test.test_name == \
         'test_pytest_snapshottest_property_test_name 2'
+
+
+@pytest.mark.parametrize('arg', ['single line string'])
+def test_pytest_snapshottest_property_test_name_parametrize_singleline(pytest_snapshot_test, arg):
+    pytest_snapshot_test.assert_match('counter')
+    assert pytest_snapshot_test.test_name == \
+        'test_pytest_snapshottest_property_test_name_parametrize_singleline[single line string] 1'
+
+
+@pytest.mark.parametrize('arg', [
+    '''
+    multi
+    line
+    string
+    '''
+])
+def test_pytest_snapshottest_property_test_name_parametrize_multiline(pytest_snapshot_test, arg):
+    pytest_snapshot_test.assert_match('counter')
+    assert pytest_snapshot_test.test_name == \
+        'test_pytest_snapshottest_property_test_name_parametrize_multiline[ multi line string ] 1'
