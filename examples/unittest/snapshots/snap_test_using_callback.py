5c91299074395a2a29ead3ddd790dd5246f5b5d4
Kamil Mankowski (kam193) <kam193@wp.pl>
2020-11-04 20:18:36 +0100

    Add example how to use callbacks

---
+++ examples/unittest/snapshots/snap_test_using_callback.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# snapshottest: v1 - https://goo.gl/zC4yUc
+from __future__ import unicode_literals
+
+from snapshottest import Snapshot
+
+
+snapshots = Snapshot()
+
+snapshots["TestUsingCallback::test_call_black 1"] = "some response to the test"
